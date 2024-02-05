ef473d8f058a267a112f778f6fa3f7729c49462e
nikhilbatgiri <nikhilbatgiri@gmail.com>
2024-02-05 22:04:56 +0530

    Initial Commit

---
+++ frontend/src/Pages/Error.js
@@ -0,0 +1,25 @@
+import { useRouteError } from "react-router-dom";
+import PageContent from "../components/PageContent";
+
+function ErrorPage(){
+    const error = useRouteError();
+
+    let title = 'An error occured!';
+    let message = 'Something went wrong!';
+
+    if(error.status === 500){
+        message = error.data.message;
+    }
+    if(error.status === 404){
+        title = 'Not Found!';
+        message = 'Could not find source or page.'
+    }
+
+    return (
+        <PageContent title={title}>
+            <p>{message}</p>
+        </PageContent>
+    )
+}
+
+export default ErrorPage;
